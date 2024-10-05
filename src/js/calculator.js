fcc5e40714604183c8981cea255904fa9ed9f7ea
Raj Chandra <accounts@rajchandra.me>
2024-10-05 09:58:03 +0000

    Fix decimal rounding issue

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,57 +1,8 @@
-//expected marks without fat
-/**var getMarksWithoutFat = function(){
-    var marksCat1 = parseFloat($('#nf-cat1').val());
-    var marksCat2 = parseFloat($('#nf-cat2').val());
-    var marksDa = parseFloat($('#nf-da').val());
-    var marksAl = parseFloat($('#nf-al').val()) || 0;
-    var marksLab = parseFloat($('#nf-lab').val());
-    var marksProj = parseFloat($('#nf-j-comp').val());
-    // var marksFat = parseFloat($('#fat').val());
-    var choice,netMarks;
-    if(marksLab && marksProj){
-        choice = 3;
-    }else if(marksLab && !marksProj){
-        choice = 2;
-    }else if(marksProj && !marksLab){
-        choice = 1;
-    }else if(!marksProj && !marksLab){
-        choice = 0; //subjects like ALA - MAT3004
-    }
-    var internals = ((marksCat1 + marksCat2)*0.5 + marksDa);
-    const marksFat = internals + (2/3*internals);
-    internals+=marksAl;
-    $('#nf-fat').val(marksFat.toFixed(1));
-    var tot = (internals >60 ? 60 : internals)+ marksFat * 0.4 ;
-    switch(choice){
-        case 0:
-            netMarks = tot;
-            break;
-        case 1:
-            netMarks = tot*0.75 + marksProj * 0.25;
-            break;
-        case 2:
-            netMarks = tot*0.75 + marksLab * 0.25;
-            break;
-        case 3:
-            netMarks = tot*0.6 + marksLab * 0.2 + marksProj * 0.2 ;
-            break;
-    }
-    if(isNaN(netMarks)){
-        $('#modal-body').html('Insufficient Data !!');
-        $('#modal-error').modal('show');
-    }
-    else{
-        $('.alert-nf-marks').show();
-        $('#nf-marks').html(`<h1 class="result-text">${netMarks.toFixed(2)}</h1>`);
-    }
-    console.log('getAds()', getAds())
-    $('.adsNFEMC').html(getAds());
+// Add this function at the beginning of the file
+function roundToTwo(num) {
+    if (isNaN(num)) return 0;
+    return Math.round((num + Number.EPSILON) * 100) / 100;
 }
-**/
-
-/*
-* Expected Marks Calculator
-* */
 
 //Expected Marks
 var getMarks = function(){
@@ -94,7 +45,7 @@ var getMarks = function(){
     }
     else{
         $('.alert-marks').show();
-        $('#marks').html(`<h1 class="result-text">${netMarks.toFixed(2)}</h1>`);
+        $('#marks').html(`<h1 class="result-text">${roundToTwo(netMarks)}</h1>`);
     }
     $('.adsEMC').html(getAds());
 }
@@ -117,7 +68,7 @@ $('#submit').click(function(){
         f="Unavailable";
     }else{
         $('.alert-cgpa-msg').show();
-        document.getElementById('cgpa-msg').innerHTML=`<h1 class="result-text">${f.toFixed(2)}</h1>`;
+        document.getElementById('cgpa-msg').innerHTML=`<h1 class="result-text">${roundToTwo(f)}</h1>`;
     }
     $('.adsQCC').html(getAds());
 });
@@ -177,16 +128,25 @@ $('#sem-cgpa-btn').on('click',function(){
         fc8 = 0;
         gpa8 = 0;
     }
-    fcgpa=((parseFloat(gpa1)*fc1)+(parseFloat(gpa2)*fc2)+(parseFloat(gpa3)*fc3)+(parseFloat(gpa4)*fc4)+(parseFloat(gpa5)*fc5)+(parseFloat(gpa6)*fc6)+(parseFloat(gpa7)*fc7)+(parseFloat(gpa8)*fc8))/(fc1*1+fc2*1+fc3*1+fc4*1+fc5*1+fc6*1+fc7*1+fc8*1);
-    if(isNaN(fcgpa)){
-        $('#modal-body').html('Insufficient Data !!');
+    
+    var totalCredits = fc1*1 + fc2*1 + fc3*1 + fc4*1 + fc5*1 + fc6*1 + fc7*1 + fc8*1;
+    
+    if (totalCredits === 0) {
+        $('#modal-body').html('Total credits cannot be zero');
         $('#modal-error').modal('show');
+    } else {
+        fcgpa = ((parseFloat(gpa1)*fc1)+(parseFloat(gpa2)*fc2)+(parseFloat(gpa3)*fc3)+(parseFloat(gpa4)*fc4)+(parseFloat(gpa5)*fc5)+(parseFloat(gpa6)*fc6)+(parseFloat(gpa7)*fc7)+(parseFloat(gpa8)*fc8)) / totalCredits;
+        
+        if(isNaN(fcgpa)){
+            $('#modal-body').html('Insufficient Data !!');
+            $('#modal-error').modal('show');
+        }
+        else{
+            $('.alert-fcgpa').show();
+            $('#fcgpa').html(`<h1 class="result-text">${roundToTwo(fcgpa)}</h1>`);
+        }
     }
-    else{
-        $('.alert-fcgpa').show();
-        $('#fcgpa').html(`<h1 class="result-text">${fcgpa.toFixed(2)}</h1>`);
-    }
-        $('.adsASCC').html(getAds());
+    $('.adsASCC').html(getAds());
 });
 
 /*
@@ -194,7 +154,26 @@ $('#sem-cgpa-btn').on('click',function(){
 * */
 $('#gbtn').on('click',function(){
     var g1=0,g2=0,g3=0,g4=0,g5=0,g6=0,g7=0,g8=0,g9=0,g10=0,c1=0,c2=0,c3=0,c4=0,c5=0,c6=0,c7=0,c8=0,c9=0,c10=0,sum=0,gpa=0;
-    g1=parseInt($('#g1').val()),g2=parseInt($('#g2').val()),g3=parseInt($('#g3').val()),g4=parseInt($('#g4').val()),g5=parseInt($('#g5').val()),g6=parseInt($('#g6').val()),g7=parseInt($('#g7').val()),g8=parseInt($('#g8').val()),g9=parseInt($('#g9').val()),g10=parseInt($('#g10').val()),c1=parseInt($('#c1').val()),c2=parseInt($('#c2').val()),c3=parseInt($('#c3').val()),c4=parseInt($('#c4').val()),c5=parseInt($('#c5').val()),c6=parseInt($('#c6').val()),c7=parseInt($('#c7').val()),c8=parseInt($('#c8').val()),c9=parseInt($('#c9').val()),c10=parseInt($('#c10').val());
+    g1=parseFloat($('#g1').val()) || 0,
+    g2=parseFloat($('#g2').val()) || 0,
+    g3=parseFloat($('#g3').val()) || 0,
+    g4=parseFloat($('#g4').val()) || 0,
+    g5=parseFloat($('#g5').val()) || 0,
+    g6=parseFloat($('#g6').val()) || 0,
+    g7=parseFloat($('#g7').val()) || 0,
+    g8=parseFloat($('#g8').val()) || 0,
+    g9=parseFloat($('#g9').val()) || 0,
+    g10=parseFloat($('#g10').val()) || 0,
+    c1=parseFloat($('#c1').val()) || 0,
+    c2=parseFloat($('#c2').val()) || 0,
+    c3=parseFloat($('#c3').val()) || 0,
+    c4=parseFloat($('#c4').val()) || 0,
+    c5=parseFloat($('#c5').val()) || 0,
+    c6=parseFloat($('#c6').val()) || 0,
+    c7=parseFloat($('#c7').val()) || 0,
+    c8=parseFloat($('#c8').val()) || 0,
+    c9=parseFloat($('#c9').val()) || 0,
+    c10=parseFloat($('#c10').val()) || 0;
     gpa=(c1*g1+c2*g2+c3*g3+c4*g4+c5*g5+c6*g6+c7*g7+c8*g8+c9*g9+c10*g10)/(c1+c2+c3+c4+c5+c6+c7+c8+c9+c10);
 
     if(isNaN(gpa)){
@@ -203,7 +182,7 @@ $('#gbtn').on('click',function(){
         gpa="Unavailable";
     }else{
         $('.alert-grades').show();
-        $('#grades').html(`<h1 class="result-text">${gpa.toFixed(2)}</h1>`);
+        $('#grades').html(`<h1 class="result-text">${roundToTwo(gpa)}</h1>`);
         // document.getElementById('reset').classList.remove("hide");
     }
     $('.adsGC').html(getAds());
@@ -251,11 +230,11 @@ $('.cgpa-input').on('keyup',function(){
 });
 
 $('.form-control').on('keyup',function(){
-    var input=document.getElementById(this.id).value;
-    if(!input==''){
-        var re=/^\d+\.?\d{0,2}$/;
-        if (!(re.test(input))){
-            $('#modal-body').html('Please enter a valid data');
+    var input = document.getElementById(this.id).value;
+    if(input !== ''){
+        var re = /^\d*\.?\d{0,2}$/;
+        if (!re.test(input)){
+            $('#modal-body').html('Please enter a valid number with up to 2 decimal places');
             $('#modal-error').modal('show');
             $(this).val('');
         }
@@ -306,33 +285,3 @@ $('.form-control').on('keyup',function(){
         $(this).val('');
     }
 });
-
-// $('#about-btn').on('click',function(){
-//     $('#about').fadeToggle('slow');
-// });
-// $('#close-btn').on('click',function(){
-//     $('#about').css({
-//         "display":" none"
-//     });
-// });
-// $('#team-view').on('click',function(){
-//     $('#team-data').fadeToggle('slow');
-// });
-
-// $('#cbtn').click(function(){
-//     document.getElementById('reset').classList.remove("hide");
-//     document.getElementById('cgpa-box').classList.remove("hide");
-//     document.getElementById('cbtn').className="hide";
-//     // $('#gbtn').attr('disabled','disabled');
-//     $('form > input').on('keyup',function(){
-//     var empty=false;
-//     $('form > input').each(function(){
-//         if($(this).val() == '')
-//             empty=true;
-//     if(empty)
-//         $('#submit').attr('disabled','disabled');
-//     else
-//         $('#submit').removeAttr('disabled');
-//     });
-// });
-
